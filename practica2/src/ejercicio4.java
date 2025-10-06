55fc4eae2755b44a838756c1a99628044a581d5f
adriangc614 <adriancsgz72@gmail.com>
2025-10-06 14:28:26 +0200

    ejercicio 4 en proceso

---
+++ practica2/src/ejercicio4.java
@@ -0,0 +1,18 @@
+public class ejercicio4 {
+
+    public static void main(String[] args) {
+        // Crea un array de números de 100 posiciones, que contendrá los números del 1 al 100.
+        // Obtén la suma de todos ellos y la media.
+        int[] a = new int[100];
+
+        for (int i = 0; i < 100; i++) {
+            a[i] = i + 1;
+        }
+
+        int suma = 0;
+        for (int i = 0; i < a.length; i++){
+            suma = suma + a[i];
+        }
+        System.out.println("La suma de todos los números es: " + suma);
+    }
+}
