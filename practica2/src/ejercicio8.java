1b406280c5aef77f5d4b25a9bde78a0d7761f845
adriangc614 <adriancsgz72@gmail.com>
2025-10-20 12:55:13 +0200

    ejercicio 8 acabado

--- practica2/src/ejercicio8.java
+++ practica2/src/ejercicio8.java
@@ -1,87 +1,51 @@
+// Programa Java para leer la altura de N personas y calcular la altura media.
+// Calcular cuántas personas tienen una altura superior a la media y cuántas tienen una altura inferior a la media.
+// El valor de N se pide por teclado y debe ser entero positivo.
+
 import java.util.Scanner;
 
 public class ejercicio8 {
 
     public static void main(String[] args) {
-        // Programa Java para leer la altura de N personas y calcular la altura media.
-        // Calcular cuántas personas tienen una altura superior a la media y cuántas tienen una altura inferior a la media.
-        // El valor de N se pide por teclado y debe ser entero positivo.
-
         Scanner sc = new Scanner(System.in);
 
-        //Crear array con el número de personas
-        System.out.println("¿De cuantas personas quieres calcular la altura media?");
+        //Introducir el número de personas que queramos medir la altura
+        System.out.println("¿De cuántas personas quieres calcular la altura media?");
         int n = sc.nextInt();
+
+        //Comprobar que n es valido
+        while (n <= 0) {
+            System.out.println("Número inválido. Debe ser positivo.");
+            System.out.println("¿De cuántas personas quieres calcular la altura media?");
+            n = sc.nextInt();
+        }
+
+        // Ahora que n es válido, creamos el array
         int[] a = new int[n];
 
         //Calcular la altura media
-        int na = 0;
-        int al = 0;
+        int at = 0;
+        int am = 0;
         for (int i = 0; i < a.length; i++) {
-            System.out.println("Introduce las alturas de las personas: ");
+            System.out.println("Introduce las alturas de las personas en cm: ");
             a[i] = sc.nextInt();
-            ;
+            at = a[i] + at;
         }
+        am = at/n;
+        System.out.println("La altura media de las personas es: " + am + "cm");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //Guardar los numeros del array leyendo por teclado
+        //Calcular cuántas personas tienen una altura superior a la media y cuántas tienen una altura inferior a la media.
+        int as = 0;
+        int ai = 0;
         for (int i = 0; i < a.length; i++) {
-            System.out.println("Introduce el valor:");
-            a[i] = sc.nextInt();
-        }
-
-        //Identificar positivos y negativos
-        int positivos = 0;
-        int negativos = 0;
-        int sumapos = 0;
-        int sumaneg = 0;
-
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] > 0) {
-                sumapos = sumapos + a[i];
-                positivos++;
-            } else if (a[i] < 0) {
-                sumaneg = sumaneg + a[i];
-                negativos++;
+            if (a[i] > am) {
+                as++; // superior a la media
+            }
+            else if (a[i] < am) {
+                ai++; // inferior a la media
             }
         }
-
-        //media positivos
-        if (positivos > 0) {
-            double mediapos = (double) sumapos / positivos;
-            System.out.println("La media de los valores positivos es: " + mediapos);
-        } else {
-            System.out.println("No hay valores positivos.");
-        }
-
-        //media negativos
-        if (negativos > 0) {
-            double medianeg = (double) sumaneg / negativos;
-            System.out.println("La media de los valores negativos es: " + medianeg);
-        } else {
-            System.out.println("No hay valores negativos.");
-        }
+        System.out.println("Número de personas con altura superior a la media: " + as);
+        System.out.println("Número de personas con altura inferior a la media: " + ai);
     }
 }
