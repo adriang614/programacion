cefffc1a608acba66762bc05325c8d6cbdf60a4b
adriang614 <adriancsgz72@gmail.com>
2025-10-24 23:44:07 +0200

    ejercicio 14 acabado

--- practica2/src/ejercicio14.java
+++ practica2/src/ejercicio14.java
@@ -10,11 +10,11 @@ public class ejercicio14 {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
 
-        //Crear lista de Strings
+        // Crear lista de Strings
         ArrayList<String> listaS = new ArrayList<>();
         String nombre;
 
-        //Pedir al usuario nombres por teclado hasta que introduzca la palabra "fin".
+        // Pedir al usuario nombres por teclado hasta que introduzca la palabra "fin".
         System.out.println("Introduce nombres (escribe fin para finalizar):");
         do {
             System.out.print("Nombre: ");
@@ -25,17 +25,24 @@ public class ejercicio14 {
             }
         } while (!nombre.equals("fin"));
 
-        //Pedir un nombre a buscar y diga si está en la lista o no, mostrando también en qué posición se encuentra si existe.
+        // Pedir un nombre a buscar
         System.out.println("¿Qué nombre quieres buscar?");
         String nombre_buscado = sc.nextLine();
 
-        //cambiar
-        if (listaS.contains(nombre_buscado)){
-            int posicion = listaS.indexOf(nombre_buscado);
-            System.out.println(nombre_buscado + " está en la lista en la posición: " + posicion);
+        // Buscar
+        int posicion = -1;
+
+        for (int i = 0; i < listaS.size(); i++) {
+            if (listaS.get(i).equals(nombre_buscado)) {
+                posicion = i;
+            }
         }
-        else {
+
+        // Mostrar resultado
+        if (posicion == -1) {
             System.out.println(nombre_buscado + " no está en la lista");
+        } else {
+            System.out.println(nombre_buscado + " está en la lista en la posición: " + posicion);
         }
     }
 }
