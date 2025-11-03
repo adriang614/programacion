d3af51c051bc360f5cc1664f9d7d6a6ee7e251d5
adriangc614 <adriancsgz72@gmail.com>
2025-11-03 14:21:26 +0100

    Ejercicio 2 en proceso

---
+++ practica3/src/ejercicio2.java
@@ -0,0 +1,19 @@
+//Escribe un programa que solicite 20 números enteros.
+// Estos números debemos de introducirlo en un array de 4 filas por 5 columnas.
+// El programa mostrará las sumas parciales de filas y en las columnas el mayor número de la columna.
+// La suma total debe aparecer en la esquina inferior derecha.
+
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        //Solicitar 20 números enteros
+
+        for (int i = 0; i < 21; i++) {
+            sc
+        }
+    }
+}
