5c074c16d2dab06b07dcc1364a21852434167b0b
adriang614 <adriancsgz72@gmail.com>
2025-11-07 01:31:09 +0100

    Ejercicio 5 acabado

--- practica3/src/ejercicio5.java
+++ practica3/src/ejercicio5.java
@@ -17,7 +17,6 @@ public class ejercicio5 {
         int[][] a = new int[filas][columnas];
 
         // Rellenar array con números que estén entre el 0 y el 1000
-        System.out.println("Introduce números enteros positivos entre el 0 y el 1000:");
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
                 int aleatorio = (int) (Math.random()*1000 + 1);
@@ -50,15 +49,29 @@ public class ejercicio5 {
 
 
         System.out.println("Suma de cada fila:");
+        int sumaTotFil = 0;
         for (int i = 0; i < filas; i++) {
             int sumaFila = 0;
             for (int j = 0; j < columnas; j++) {
                 sumaFila += a[i][j];
             }
+            sumaTotFil += sumaFila;
             System.out.println("Fila " + i + ": " + sumaFila);
         }
+        System.out.println ("La suma total de las filas es: " + sumaTotFil);
 
+        System.out.println("Suma de cada columna: ");
+        int sumaTotCol = 0;
+        for (int j = 0; j < columnas; j++) {
+            int sumaCol = 0;
+            for (int i = 0; i < filas; i++) {
+                sumaCol += a[i][j];
+            }
+            sumaTotCol += sumaCol;
+            System.out.println("Columna " + j + ": " + sumaCol);
+        }
+        System.out.println ("La suma total de las columnas es: " + sumaTotFil);
 
-
+        System.out.println ("La suma total de las filas y las columnas es: " + (sumaTotCol + sumaTotFil));
     }
 }
