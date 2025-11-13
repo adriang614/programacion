a1dba27c46e0e081e1f7a9cb5b4ed7b62e3ffa97
adriang614 <adriancsgz72@gmail.com>
2025-11-14 00:23:43 +0100

    prueba1 update

--- Pruebas/src/prueba1.java
+++ Pruebas/src/prueba1.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class prueba1 {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+
+        Scanner s = new Scanner(System.in);
+        int[] a = new int[10];
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Introduce el número " + i + " : ");
+            a[i] = s.nextInt();
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Valor " + a[i] + " en el índice " + i);
+        }
+
     }
 }
