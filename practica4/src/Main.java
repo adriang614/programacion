018c5b0c27d55f83fef66f188efffacde4acc3c4
adriangc614 <adriancsgz72@gmail.com>
2025-11-24 14:25:11 +0100

    trabajo en practica4 dia 24-11-2025

---
+++ practica4/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        //Introducir el porcentaje de retenecion
+        System.out.println("¿Qué porcentaje de retención quieres aplicarle al sueldo?: ");
+        Scanner ret = new Scanner(System.in);
+        retencion = ret.nextDouble();
+        //Mostrar sueldo neto
+        System.out.println("Sueldo neto: ");
+    }
+}
