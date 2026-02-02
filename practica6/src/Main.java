07a71041ab6f13f113503864155dac92b9d3c7b0
adriangc614 <adriancsgz72@gmail.com>
2026-02-02 14:10:50 +0100

    Creacion practica6

--- practica6/src/Main.java
+++ practica6/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public void main (String[] args){
-
-        System.out.println("asdfsadf");
+        while (true){
+            System.out.println("");
+        }
     }
 }
