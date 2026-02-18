8eeaa30675c8a473dfaf36114ff5acc51cbb6270
adriang614 <adriancsgz72@gmail.com>
2026-02-18 23:33:46 +0100

    comienzo practicando para practica6

---
+++ repaso_practica6/src/bloque1/a1.java
@@ -0,0 +1,12 @@
+package bloque1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class a1 {
+    //Crea un ArrayList llamado registroCombate. Añade 5
+    //eventos (ej: "Orco derrotado", "Poción usada"). Luego, muestra el tercer evento que
+    //ocurrió.
+
+    List <String> registroCombate = new ArrayList<>();
+}
