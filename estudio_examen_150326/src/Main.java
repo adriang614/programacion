0ec3513e37cde8984b823e27c5619f18ea3d7254
adriang614 <adriancsgz72@gmail.com>
2026-03-16 03:17:18 +0100

    estudio

---
+++ estudio_examen_150326/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main (String[] args) {
+        Bloque3 bl3 = new Bloque3();
+
+        bl3.leerJson("estudio_examen_150326/files/personas.txt");
+    }
+}
